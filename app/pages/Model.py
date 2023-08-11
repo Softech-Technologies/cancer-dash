82d0047ea6c6e72b527fb931399ea59f3a065a49
jamie-codez <cruiseomondi90@gmail.com>
2023-08-12 00:32:06 +0300

    Landed in parfect data

--- app/pages/Model.py
+++ app/pages/Model.py
@@ -1,10 +1,7 @@
 import streamlit as st
 
 st.set_page_config(page_title='Predictive Model', page_icon=':hospital:', layout='wide')
-st.title('Cancer Dashboard')
-st.write('This is a dashboard for cancer data exploration and visualization')
-st.sidebar.title('Cancer Dashboard')
-st.markdown('This is a dashboard for cancer data exploration and visualization')
+st.title('Prediction Model')
 
 hide_streamlit_style = """
             <style>
@@ -13,3 +10,4 @@ hide_streamlit_style = """
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
