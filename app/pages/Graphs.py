e89b3a6e17cec5517fe747ff5e2b513a560a94b6
jamie-codez <cruiseomondi90@gmail.com>
2023-08-12 14:53:50 +0300

    All we need is here

--- app/pages/Graphs.py
+++ app/pages/Graphs.py
@@ -33,7 +33,7 @@ breast_cancer_df["target"] = [breast_cancer.target_names[val] for val in breast_
 
 # st.set_page_config(layout="wide")
 
-st.markdown("## Breast Cancer Dataset Analysis")  ## Main Title
+st.markdown("## Cancer Dataset Analysis")  ## Main Title
 
 ################# Scatter Chart Logic #################
 
@@ -128,32 +128,55 @@ if hexbin_x_axis and hexbin_y_axis:
 
 data_url = os.path.join(os.path.dirname(__file__),'..','data','clean_data.csv')
 data_frame = pd.read_csv(data_url)
-ages = data_frame['Patient\'s Age'].value_counts()
+st.write("Age distribution:")
+plt.figure(figsize=(5,4))
+ages = data_frame['Patient\'s Age'].plot(kind="density",color="r")
+plt.xlabel('Patient\'s Age')
+plt.ylabel('Count')
+plt.title('Patient\'s Age vs Count')
+plt.show()
+st.pyplot(plt)
 
-st.sidebar.markdown("### Bar Chart: Average Measurements Per Tumor Type : ")
+############## Pie chart ##########################
+# plt.title("Gender distribution")
+# pie = data_frame["Patient's Gender"].value_counts().plot(kind='pie', autopct='%1.1f%%', shadow=True, startangle=90)
+# # plt.show()
+# st.pyplot(pie)
+genders = data_frame['Patient\'s Gender'].value_counts()
+labels = data_frame['Patient\'s Gender'].unique()
 
-avg_breast_cancer_df = breast_cancer_df.groupby("target").mean()
-barf_axis = st.sidebar.multiselect(label="Average Measures per Tumor Type Bar Chart 1",
-                                   options=measurements,
-                                   default=["mean radius", "mean texture", "mean perimeter", "area error"])
+st.write("Gender distribution:")
+fig, ax = plt.subplots()
+ax.pie(genders, labels=labels, autopct='%1.1f%%', startangle=90)
+ax.axis('equal')  # Equal aspect ratio ensures the pie chart is circular.
+st.pyplot(fig)
 
-if bar_axis:
-    barf = plt.figure(figsize=(6, 4))
+st.write("Breast cancer Detail Analysis")
 
-    barf_ax = barf.add_subplot(111)
-
-    sub_avg_breast_cancer_df = avg_breast_cancer_df[barf_axis]
-
-    sub_avg_breast_cancer_df.plot.bar(alpha=0.8, ax=barf_ax, title="Average Measurements per Tumor Type");
-
-else:
-    barf_fig = plt.figure(figsize=(6, 4))
-
-    barf_ax = bar_fig.add_subplot(111)
-
-    sub_avg_breast_cancer_df = avg_breast_cancer_df[["mean radius", "mean texture", "mean perimeter", "area error"]]
-
-    sub_avg_breast_cancer_df.plot.bar(alpha=0.8, ax=barf_ax, title="Average Measurements per Tumor Type");
+# st.sidebar.markdown("### Bar Chart: Average Measurements Per Tumor Type : ")
+#
+# avg_breast_cancer_df = breast_cancer_df.groupby("target").mean()
+# barf_axis = st.sidebar.multiselect(label="Average Measures per Tumor Type Bar Chart 1",
+#                                    options=measurements,
+#                                    default=["mean radius", "mean texture", "mean perimeter", "area error"])
+#
+# if bar_axis:
+#     barf = plt.figure(figsize=(6, 4))
+#
+#     barf_ax = barf.add_subplot(111)
+#
+#     sub_avg_breast_cancer_df = avg_breast_cancer_df[barf_axis]
+#
+#     sub_avg_breast_cancer_df.plot.bar(alpha=0.8, ax=barf_ax, title="Average Measurements per Tumor Type");
+#
+# else:
+#     barf_fig = plt.figure(figsize=(6, 4))
+#
+#     barf_ax = bar_fig.add_subplot(111)
+#
+#     sub_avg_breast_cancer_df = avg_breast_cancer_df[["mean radius", "mean texture", "mean perimeter", "area error"]]
+#
+#     sub_avg_breast_cancer_df.plot.bar(alpha=0.8, ax=barf_ax, title="Average Measurements per Tumor Type");
 ##################### Layout Application ##################
 
 container1 = st.container()
@@ -176,6 +199,6 @@ with container2:
 
 container3 = st.container()
 col5, col6 = st.columns(2)
-with container3:
-    with col5:
-        bar_fig
+# with container3:
+#     with col5:
+
